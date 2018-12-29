481a232df71a3521a35d9a932e063dbc8193a910
Dmitriy Kireev <chief_programmer@mail.ru>
2018-12-29 17:51:46 +0300

    add buttons in TodoListItem.js

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -12,7 +12,19 @@ const TodoListItem = ({ label, important = false }) => {
       <span className="todo-list-item-label" style={style}>
         {label}
       </span>
-      ;
+      <button
+        type="button"
+        className="btn btn-outline-success btn-sm float-right"
+      >
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button
+        type="button"
+        className="btn btn-outline-danger btn-sm float-right"
+      >
+        <i className="fa fa-trash-o" />
+      </button>
     </span>
   );
 };
